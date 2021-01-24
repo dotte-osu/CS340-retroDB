5f7e666101ec1d3305624f6194e61e1db37d0f2f
xHoudek <xhoudek@gmail.com>
2021-01-24 13:19:10 -0600

    Set up files and layout for project

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mysql = require('./dbcon.js');
+
+const app = express();
+const handlebars = require('express-handlebars');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const path = require('path');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine(
+	'handlebars',
+	handlebars({
+		defaultLayout: 'main',
+		layoutsDir: __dirname + '/views/layouts/',
+		partialsDir: __dirname + '/views/partials/'
+	})
+);
+
+app.set('view engine', 'handlebars');
+const port = 54321; // TODO: see if port is open and change if necessary
+
+// routes
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+// error routes
+app.use((req, res) => {
+	res.status(404);
+	res.render('404');
+});
+
+app.use((req, res) => {
+	res.status(500);
+	res.render('500');
+});
+
+// start server
+app.listen(port, () => {
+	console.log(`Express started on http://flipX.engr.oregonstate.edu:${port}; press Ctrl-C to terminate.`);
+});
---
+++ dbcon.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+
+  //TODO: fill out db info
+  host            : '',
+  user            : '',
+  password        : '',
+  database        : ''
+});
+
+module.exports.pool = pool;
