582e3a2b5b3c3d319c7cf24db5f81093e447a46e
Eriko Dott <dotte@flip3.engr.oregonstate.edu>
2021-01-25 17:36:18 -0800

    initial commit

--- dbcon.js
+++ dbcon.js
@@ -3,10 +3,10 @@ var pool = mysql.createPool({
   connectionLimit : 10,
 
   //TODO: fill out db info
-  host            : '',
-  user            : '',
-  password        : '',
-  database        : ''
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_dotte',
+  password: '8814',
+  database: 'cs340_dotte'
 });
 
 module.exports.pool = pool;
