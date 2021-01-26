9c8e5f0fcb4a5a274ba49fc659e02e3a6bc2a950
Eriko Dott <dotte@flip3.engr.oregonstate.edu>
2021-01-25 17:49:32 -0800

    removed credentials, updated readme

--- dbcon.js
+++ dbcon.js
@@ -3,10 +3,10 @@ var pool = mysql.createPool({
   connectionLimit : 10,
 
   //TODO: fill out db info
-  host: 'classmysql.engr.oregonstate.edu',
-  user: 'cs340_dotte',
-  password: '8814',
-  database: 'cs340_dotte'
+  host: '',
+  user: '',
+  password: '',
+  database: ''
 });
 
 module.exports.pool = pool;
