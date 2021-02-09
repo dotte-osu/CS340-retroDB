ddf679db47d8935dc7fff011d38538f4cbd0714d
xHoudek <xhoudek@gmail.com>
2021-02-09 08:37:29 -0600

    Changed admin layout to separate data for each table

--- routes/users.js
+++ routes/users.js
@@ -11,41 +11,41 @@ module.exports = (function() {
 				console.log('Failed to fetch User:', error);
 				res.end();
 			}
-            // check if username exists
-            if (!results[0]) {
-                context.failed = true
-            } else {
-                context.username = results[0].username;
-                context.firstName = results[0].firstName;
-                context.lastName = results[0].lastName;
-                // TODO: context.user = true IF :username is logged in username
-            }
+			// check if username exists
+			if (!results[0]) {
+				context.failed = true;
+			} else {
+				context.username = results[0].username;
+				context.firstName = results[0].firstName;
+				context.lastName = results[0].lastName;
+				// TODO: context.user = true IF :username is logged in username
+			}
 			complete();
 		});
 	}
 
-    function getListsByUsername(req, res, mysql, context, complete) {
-        const username = req.params.username;
-        const sqlQuery = 'SELECT l.listID, l.listName ' +
-        'FROM Lists l ' +
-        'INNER JOIN Users u ON l.createdBy = u.userID AND u.userID = ' +
-        '(SELECT userID FROM Users WHERE username = ?)'
-        mysql.pool.query(sqlQuery, username, function(error, results, fields) {
-            if (error) {
+	function getListsByUsername(req, res, mysql, context, complete) {
+		const username = req.params.username;
+		const sqlQuery =
+			'SELECT l.listID, l.listName ' +
+			'FROM Lists l ' +
+			'INNER JOIN Users u ON l.createdBy = u.userID AND u.userID = ' +
+			'(SELECT userID FROM Users WHERE username = ?)';
+		mysql.pool.query(sqlQuery, username, function(error, results, fields) {
+			if (error) {
 				console.log('Failed to fetch Lists:', error);
 				res.end();
 			}
 
-            // check if username exists
-            if (!results[0]) {
-                context.failed = true
-            } else {
-
-                context.lists = results
-            }
-            complete();
-        })
-    }
+			// check if username exists
+			if (!results[0]) {
+				context.failed = true;
+			} else {
+				context.lists = results;
+			}
+			complete();
+		});
+	}
 
 	userRouter.get('/login', function(req, res) {
 		res.render('login', { login: true, style: 'login.css' });
@@ -56,27 +56,62 @@ module.exports = (function() {
 	});
 
 	userRouter.get('/admin', function(req, res) {
-		res.render('admin', { admin: true, style: 'admin.css' });
+		context = {
+			admin: true,
+            message: true
+		};
+		res.render('admin', context);
+	});
+
+    userRouter.get('/admin/games', function(req, res) {
+		context = {
+			admin: true,
+            gamesActive: 'active'
+		};
+		res.render('admin', context);
+	});
+
+    userRouter.get('/admin/consoles', function(req, res) {
+		context = {
+			admin: true,
+            consolesActive: 'active'
+		};
+		res.render('admin', context);
+	});
+
+    userRouter.get('/admin/publishers', function(req, res) {
+		context = {
+			admin: true,
+            publishersActive: 'active'
+		};
+		res.render('admin', context);
+	});
+
+    userRouter.get('/admin/users', function(req, res) {
+		context = {
+			admin: true,
+            usersActive: 'active'
+		};
+		res.render('admin', context);
 	});
 
 	userRouter.get('/:username', function(req, res) {
-        var callbackCount = 0;
+		var callbackCount = 0;
 		var context = {};
 		getUserByUsername(req, res, mysql, context, complete);
-        getListsByUsername(req, res, mysql, context, complete)
-        function complete() {
+		getListsByUsername(req, res, mysql, context, complete);
+		function complete() {
 			callbackCount++;
 			if (callbackCount >= 2) {
-                if (context.failed) {
-                    console.log('Username not found:', req.params.username)
-                    res.redirect('/')
-                } else {
-                    console.log(context)
-                    res.render('user', context);
-                }
+				if (context.failed) {
+					console.log('Username not found:', req.params.username);
+					res.redirect('/');
+				} else {
+					console.log(context);
+					res.render('user', context);
+				}
 			}
 		}
-		
 	});
 
 	userRouter.post('/login', function(req, res) {
