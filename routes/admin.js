c5fa3787cf4238b888491cf474017610573cb816
xHoudek <xhoudek@gmail.com>
2021-02-09 11:16:28 -0600

    Admin pages now get info from DB

--- routes/admin.js
+++ routes/admin.js
@@ -3,44 +3,124 @@ module.exports = (function() {
 	const adminRouter = express.Router();
 	const mysql = require('../dbcon.js');
 
-    adminRouter.get('/', function(req, res) {
-		context = {
+	function getAllGames(req, res, mysql, context, complete) {
+		const sqlQuery = 'SELECT * FROM Games';
+		mysql.pool.query(sqlQuery, function(error, results, fields) {
+			if (error) {
+				console.log('Failed to fetch Games:', error);
+				res.end();
+			}
+
+			context.games = results;
+			complete();
+		});
+	}
+
+	function getAllConsoles(req, res, mysql, context, complete) {
+		const sqlQuery = 'SELECT * FROM Consoles';
+		mysql.pool.query(sqlQuery, function(error, results, fields) {
+			if (error) {
+				console.log('Failed to fetch Consoles:', error);
+				res.end();
+			}
+
+			context.consoles = results;
+			complete();
+		});
+	}
+
+    function getAllPublishers(req, res, mysql, context, complete) {
+		const sqlQuery = 'SELECT * FROM Publishers';
+		mysql.pool.query(sqlQuery, function(error, results, fields) {
+			if (error) {
+				console.log('Failed to fetch Publishers:', error);
+				res.end();
+			}
+
+			context.publishers = results;
+			complete();
+		});
+	}
+
+    function getAllUsers(req, res, mysql, context, complete) {
+		const sqlQuery = 'SELECT * FROM Users';
+		mysql.pool.query(sqlQuery, function(error, results, fields) {
+			if (error) {
+				console.log('Failed to fetch Users:', error);
+				res.end();
+			}
+
+			context.users = results;
+			complete();
+		});
+	}
+
+	adminRouter.get('/', function(req, res) {
+		var context = {
 			admin: true,
-            message: true
+			message: true
 		};
 		res.render('admin', context);
 	});
 
-    adminRouter.get('/games', function(req, res) {
-		context = {
+	adminRouter.get('/games', function(req, res) {
+		var callbackCount = 0;
+		var context = {
 			admin: true,
-            gamesActive: 'active'
+			gamesActive: 'active'
 		};
-		res.render('admin', context);
+		getAllGames(req, res, mysql, context, complete);
+		function complete() {
+			callbackCount++;
+			if (callbackCount >= 1) {
+				res.render('admin', context);
+			}
+		}
 	});
 
-    adminRouter.get('/consoles', function(req, res) {
-		context = {
+	adminRouter.get('/consoles', function(req, res) {
+		var callbackCount = 0;
+		var context = {
 			admin: true,
-            consolesActive: 'active'
+			consolesActive: 'active'
 		};
-		res.render('admin', context);
+		getAllConsoles(req, res, mysql, context, complete);
+		function complete() {
+			callbackCount++;
+			if (callbackCount >= 1) {
+				res.render('admin', context);
+			}
+		}
 	});
 
-    adminRouter.get('/publishers', function(req, res) {
-		context = {
+	adminRouter.get('/publishers', function(req, res) {
+		var callbackCount = 0;
+		var context = {
 			admin: true,
-            publishersActive: 'active'
+			publishersActive: 'active'
 		};
-		res.render('admin', context);
+		getAllPublishers(req, res, mysql, context, complete);
+		function complete() {
+			callbackCount++;
+			if (callbackCount >= 1) {
+				res.render('admin', context);
+			}
+		}
 	});
 
-    adminRouter.get('/users', function(req, res) {
-		context = {
+	adminRouter.get('/users', function(req, res) {
+		var callbackCount = 0;
+		var context = {
 			admin: true,
-            usersActive: 'active'
+			usersActive: 'active'
 		};
-		res.render('admin', context);
+		getAllUsers(req, res, mysql, context, complete);
+		function complete() {
+			callbackCount++;
+			if (callbackCount >= 1) {
+				res.render('admin', context);
+			}
+		}
 	});
 
 	return adminRouter;
