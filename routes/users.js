0fe8560631508afd16ebe48789255b07d3cd1bcf
xHoudek <xhoudek@gmail.com>
2021-02-14 18:20:44 -0600

    Created INSERT functionality for registering a user

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ module.exports = (function() {
 	const express = require('express');
 	const userRouter = express.Router();
 	const mysql = require('../dbcon.js');
+	const bcrypt = require('bcrypt')
 
 	function getUserByUsername(req, res, mysql, context, complete) {
 		const username = req.params.username;
@@ -37,20 +38,22 @@ module.exports = (function() {
 				res.end();
 			}
 
-			// check if username exists
-			if (!results[0]) {
-				context.failed = true;
-			} else {
-				context.lists = results;
-			}
+			context.lists = results;
 			complete();
 		});
 	}
 
+	function checkPassword(password, confirmPassword) {
+		if (8 <= password.length && password.length <= 20 && password == confirmPassword) {
+			return true;
+		}
+		return false;
+	}
+
 	userRouter.get('/', function(req, res) {
 		// TODO: redirect to user's page, for now redirect to test
-		res.redirect('/user/test')
-	})
+		res.redirect('/user/test');
+	});
 
 	userRouter.get('/login', function(req, res) {
 		res.render('login', { login: true, style: 'login.css' });
@@ -72,7 +75,6 @@ module.exports = (function() {
 					console.log('Username not found:', req.params.username);
 					res.redirect('/');
 				} else {
-					console.log(context);
 					res.render('user', context);
 				}
 			}
@@ -93,22 +95,36 @@ module.exports = (function() {
 		res.redirect('/user');
 	});
 
-	userRouter.post('/register', function(req, res) {
-		const { username, firstName, lastName, email, password, confirmPassword } = req.body;
+	// creates a new user and redirects to user page
+	userRouter.post('/register', async function(req, res) {
 
-		// debug
-		let userInfo = {
-			username: username,
-			firstName: firstName,
-			lastName: lastName,
-			email: email,
-			password: password,
-			confirmPassword: confirmPassword
-		};
-		console.log(userInfo);
-		// end debug
+		if (checkPassword(req.body.password, req.body.confirmPassword)) {
 
-		res.redirect('/user');
+			// create hash
+			const hash = await bcrypt.hash(req.body.password, 12);
+
+			// create user
+			const sqlQuery =
+				'INSERT INTO Users (username, firstName, lastName, email, password) VALUES (?, ?, ?, ?, ?)';
+			const inserts = [ req.body.username, req.body.firstName, req.body.lastName, req.body.email, hash ];
+
+			mysql.pool.query(sqlQuery, inserts, function(error, results, fields) {
+				if (error) {
+					console.log(error);
+					context = { register: true, error: true}
+					res.render('register', context)
+				} else {
+					// redirect to user page
+					
+					res.redirect('/user/' + req.body.username);
+				}
+				
+			});
+		} else {
+			console.log('Bad password');
+			context = { register: true, error: true}
+			res.render('register', context)
+		}
 	});
 
 	return userRouter;
