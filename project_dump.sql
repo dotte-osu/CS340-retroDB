f6d81558ffe82e39e02cebe902147428d193aa76
Eriko Dott <dotte@flip3.engr.oregonstate.edu>
2021-02-21 15:00:36 -0800

    fixed query

--- project_dump.sql
+++ project_dump.sql
@@ -1,21 +1,4 @@
 -- Dummy dump data
--- Create Games table
-DROP TABLE IF EXISTS `Games`;
-CREATE TABLE `Games` (
- `gameID` int(11) not null AUTO_INCREMENT,
- `gameName` varchar(255) not null,
- `gameReleaseYear` int(11),
- `consoleID` int(11) not null,
- `publisherID` int(11),
-  PRIMARY KEY (`gameID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- intert dummy data
-INSERT INTO `Games`(`gameName`, `gameReleaseYear`, `consoleID`, `publisherID`) 
-VALUES ('Super Mario',1985,1,1),('Dr. Mario 64',2001,2,1),
-('Aerobiz Supersonic',1994,3,2),('Sonic the Hedgehog',1991,3,2),
-('Sonic Spinball',1993,3,2),('E.T. the Extra-Terrestrial',1982,4,3);
-
 
 -- Create Publishers table
 DROP TABLE IF EXISTS `Publishers`;
@@ -52,35 +35,24 @@ VALUES ('Super Nintendo Entertainment System','Nintendo','Home Console'),
 ('Sega Genesis','Sega','Home Console'),
 ('Atari 2600','Atari','Home Console');
 
--- Create Lists table
-DROP TABLE IF EXISTS `Lists`;
-CREATE TABLE `Lists` (
-  `listID` int(11) NOT NULL AUTO_INCREMENT,
-  `listName` varchar(255) NOT NULL,
-  `listDescription` varchar(255),
-  `lastUpdated` date,
-  `createdBy` int(11),
-  PRIMARY KEY (`listID`),
-  CONSTRAINT `createdBy_fk` FOREIGN KEY (`createdBy`) REFERENCES `Users` (`userID`)
+-- Create Games table
+DROP TABLE IF EXISTS `Games`;
+CREATE TABLE `Games` (
+ `gameID` int(11) not null AUTO_INCREMENT,
+ `gameName` varchar(255) not null,
+ `gameReleaseYear` int(11),
+ `consoleID` int(11) not null,
+ `publisherID` int(11),
+  PRIMARY KEY (`gameID`),
+  CONSTRAINT `console_fk` FOREIGN KEY (`consoleID`) REFERENCES `Consoles` (`consoleID`),
+  CONSTRAINT `publisher_fk` FOREIGN KEY (`publisherID`) REFERENCES `Publishers` (`publisherID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- insert dummy data
-INSERT INTO `Lists` (`listName`, `listDescription`, `lastUpdated`)
-VALUES ('My Favorites', 'My favorite games', '2020-02-02'),
-('Want to play', 'all of the games that I want to play', '2020-01-29');
-
--- Create GamesLists table
-DROP TABLE IF EXISTS `GamesLists`;
-CREATE TABLE `GamesLists` (
-  `listID` int(11) NOT NULL,
-  `gameID` int(11) NOT NULL,
-  CONSTRAINT `list_fk` FOREIGN KEY (`listID`) REFERENCES `Lists` (`listID`),
-  CONSTRAINT `game_fk` FOREIGN KEY (`gameID`) REFERENCES `Games` (`gameID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- insert dummy data
-INSERT INTO `GamesLists`(`listID`, `gameID`)
-VALUES (1,1),(1,2),(1,4),(2,3),(2,5),(2,6)
+-- intert dummy data
+INSERT INTO `Games`(`gameName`, `gameReleaseYear`, `consoleID`, `publisherID`) 
+VALUES ('Super Mario',1985,1,1),('Dr. Mario 64',2001,2,1),
+('Aerobiz Supersonic',1994,3,2),('Sonic the Hedgehog',1991,3,2),
+('Sonic Spinball',1993,3,2),('E.T. the Extra-Terrestrial',1982,4,3);
 
 -- Create Users table
 DROP TABLE IF EXISTS `Users`;
@@ -101,3 +73,35 @@ INSERT INTO `Users`(`username`, `firstName`, `lastName`, `email`, `password`)
 VALUES ('test', 'Foo', 'Bar', 'fake@email.io', 'password1');
 INSERT INTO `Users` (`username`, `email`, `password`)
 VALUES ('noname', 'mystery@aol.com', 'password1');
+
+-- Create Lists table
+DROP TABLE IF EXISTS `Lists`;
+CREATE TABLE `Lists` (
+  `listID` int(11) NOT NULL AUTO_INCREMENT,
+  `listName` varchar(255) NOT NULL,
+  `listDescription` varchar(255),
+  `lastUpdated` date,
+  `createdBy` int(11),
+  PRIMARY KEY (`listID`),
+  CONSTRAINT `createdBy_fk` FOREIGN KEY (`createdBy`) REFERENCES `Users` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- insert dummy data
+INSERT INTO `Lists` (`listName`, `listDescription`, `lastUpdated`, `createdBy`)
+VALUES ('My Favorites', 'My favorite games', '2020-02-02', 1),
+('Want to play', 'all of the games that I want to play', '2020-01-29', 2),
+('My fav game', 'this is literally the best game ever made', '2020-02-07', 2);
+
+-- Create GamesLists table
+DROP TABLE IF EXISTS `GamesLists`;
+CREATE TABLE `GamesLists` (
+  `listID` int(11) NOT NULL,
+  `gameID` int(11) NOT NULL,
+  CONSTRAINT `list_fk` FOREIGN KEY (`listID`) REFERENCES `Lists` (`listID`),
+  CONSTRAINT `game_fk` FOREIGN KEY (`gameID`) REFERENCES `Games` (`gameID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- insert dummy data
+INSERT INTO `GamesLists`(`listID`, `gameID`)
+VALUES (1,1),(1,2),(1,4),(2,3),(2,5),(2,6),(3,6);
+
